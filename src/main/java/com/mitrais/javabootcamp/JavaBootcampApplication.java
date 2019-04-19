a560c620afa2553a62ff448c43f353d4c2740a0c
latudimas <riswandha.ld@gmail.com>
2019-04-19 07:52:43 +0700

    first commit

---
+++ src/main/java/com/mitrais/javabootcamp/JavaBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.mitrais.javabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootcampApplication.class, args);
+	}
+
+}
